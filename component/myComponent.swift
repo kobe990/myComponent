5b73951dabb2835e7131e823914ab612ef7f9121
kobe990 <125108977@qq.com>
2016-03-23 21:03:44 +0800

    init

---
+++ component/myComponent.swift
@@ -0,0 +1,15 @@
+//
+//  myComponent.swift
+//  component
+//
+//  Created by zxa on 16/3/23.
+//  Copyright © 2016年 zxa. All rights reserved.
+//
+
+import UIKit
+
+class myComponent {
+    func myprint() {
+        print("this is zxa")
+    }
+}
