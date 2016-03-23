78e234a10a73fc7c38ac69805fb53a9708b5deed
稀昂 <xiang.zxa@alipay.com>
2016-03-23 20:51:17 +0800

    Initial Commit

---
+++ component/component.h
@@ -0,0 +1,19 @@
+//
+//  component.h
+//  component
+//
+//  Created by zxa on 16/3/23.
+//  Copyright © 2016年 zxa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for component.
+FOUNDATION_EXPORT double componentVersionNumber;
+
+//! Project version string for component.
+FOUNDATION_EXPORT const unsigned char componentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <component/PublicHeader.h>
+
+
